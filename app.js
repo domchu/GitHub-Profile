a2d292d762a685b81957edb360063d075f65a0e0
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-07-03 23:32:47 +0100

    added the scripts text

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ async function getUser(user) {
 function createUserCard(user) {
   const card = document.createElement("div");
   card.classList.add("card");
+
+  card.innerHTML = `
+     <div>
+     <img src ="${user.avatar_url}" alt="${user.name}" />
+     </div>
+     <div>
+     <h2>${user.name}</h2>
+     <p>${user.bio}</p>
+     </div>
+    `;
 }
