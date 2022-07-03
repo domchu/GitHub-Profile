ab817b90d5ec73cb7b8a3c9dba11c8a4c2266837
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-07-04 00:56:44 +0100

    added text

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-getUser("domchu");
+// getUser("domchu");
 
 async function getUser(user) {
   const resp = await fetch(api + user);
