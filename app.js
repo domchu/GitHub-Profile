9fa7c48353cebaaa43f2c2209e628c9b502c3fdc
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-07-03 23:54:00 +0100

    added more scripts

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const api = "https://api.github.com/users/";
 
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 async function getUser(user) {
   const resp = await fetch(api + user);
 
@@ -11,13 +15,32 @@ function createUserCard(user) {
   const card = document.createElement("div");
   card.classList.add("card");
 
-  card.innerHTML = `
+  const cardHTML = `
+<div class="card">
      <div>
      <img src ="${user.avatar_url}" alt="${user.name}" />
      </div>
      <div>
      <h2>${user.name}</h2>
      <p>${user.bio}</p>
+
+     <ul>
+     <li>${user.followers}</li>
+     <li>${user.following}</li>
+     <li>${user.public_repos}</li>
+     </ul>
+     </div>
      </div>
     `;
+  main.innerHTML = cardHTML;
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+  if (user) {
+    getUser(user);
+    search.value = "";
+  }
+});
