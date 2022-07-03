d9524d565195c11bc6e8acd8e7dddde5a5fb640e
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-07-03 23:18:44 +0100

    added the profile structure

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const api = "https://api.github.com/users/";
 
 async function getUser(user) {
   const resp = await fetch(api + user);
+
+  const respData = await resp.json();
+  createUserCard(respData);
+}
+
+function createUserCard(user) {
+  const card = document.createElement("div");
+  card.classList.add("card");
 }
